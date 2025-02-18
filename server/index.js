37befdf76d8e6470ec5825180133abc24172cba3
Akshay S <62248044+blitzster@users.noreply.github.com>
2025-02-18 10:44:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -39,6 +39,8 @@ app.use('/api/posts', postRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
-
+app.get('/', (req, res) => {
+    res.json({ status: "ok" });
+});
 connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000, () => console.log(`Server started on port ${process.env.PORT}`))).catch(error => {console.log(error)})
 
